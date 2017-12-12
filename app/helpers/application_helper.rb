e69e52c1f55db84bf3d8a644fdf4e7c3c5eac539
Jordan Hudgens <jordan@devcamp.com>
2017-12-12 17:42:33 +0000

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,15 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path)
     end
   end
-end
 
   def source_helper(layout_name)
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+  
+  def copyright_generator
+    @copyright = BradleyViewTool::Renderer.copyright 'Shane Bradley', 'All rights reserved'
   end
 end
