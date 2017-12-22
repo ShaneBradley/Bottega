7019cdfa52101d8226bd7a1fbae6c6d6457c3150
Jordan Hudgens <jordan@devcamp.com>
2017-12-22 18:26:00 +0000

    javascript fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jquery3
+//= require popper
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
@@ -21,4 +21,3 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-
